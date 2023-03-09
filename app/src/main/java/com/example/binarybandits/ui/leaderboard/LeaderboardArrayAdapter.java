3186fc9242642bb022410818e3a25c54f57cf11c
Logan11111 <Hepfnerl344@gmail.com>
2023-03-08 20:40:07 -0700

    added initial functionality of leaderboard list

---
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardArrayAdapter.java
@@ -0,0 +1,40 @@
+package com.example.binarybandits.ui.leaderboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.binarybandits.R;
+import com.example.binarybandits.models.Player;
+
+import java.util.ArrayList;
+
+public class LeaderboardArrayAdapter extends ArrayAdapter {
+        public LeaderboardArrayAdapter(Context context, ArrayList<Player> players) {
+            super(context, 0, players);
+        }
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup
+                parent) {
+            View view;
+            if (convertView == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_leaderboard_items,
+                        parent, false);
+            } else {
+                view = convertView;
+            }
+            // add items to listview
+            Player player = (Player) getItem(position);
+            TextView playerName = view.findViewById(R.id.player_name_text);
+            TextView score = view.findViewById((R.id.score_Player));
+            playerName.setText(player.getUsername());
+            return view;
+        }
+    }
+
