bbd9585a3aa39f8a2700fe3c3b69ea378dbd1819
Logan11111 <Hepfnerl344@gmail.com>
2023-03-08 20:33:19 -0700

    added initial functionality of leaderboard list

--- app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardFragment.java
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardFragment.java
@@ -1,18 +1,40 @@
 package com.example.binarybandits.ui.leaderboard;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
 
 import com.example.binarybandits.R;
+import com.example.binarybandits.models.Player;
+
+import java.util.ArrayList;
 
 public class LeaderboardFragment extends Fragment {
+
+    private ArrayList<Player> players;
+    private LeaderboardViewModel leaderboardViewModel;
+    private View leaderboard;
+    private ArrayAdapter<Player> playerArrayAdapter;
+    private ListView playerList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        leaderboardViewModel = new LeaderboardViewModel();
+        leaderboardViewModel.sortPlayer_list();
+        players = leaderboardViewModel.getPlayerList();
+        leaderboard = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        playerList = leaderboard.findViewById(R.id.playerList);
+        playerArrayAdapter = new LeaderboardArrayAdapter(getActivity(), players);
+        playerList.setAdapter(playerArrayAdapter);
+        return leaderboard;
     }
+
+
 }
--- app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardViewModel.java
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardViewModel.java
@@ -1,14 +1,59 @@
 package com.example.binarybandits.ui.leaderboard;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.binarybandits.models.Player;
+
+import java.util.ArrayList;
+
 public class LeaderboardViewModel extends ViewModel {
     private final MutableLiveData<String> mText;
+    private Player key;
+    private Player temp;
+    private ArrayList<Player> sortedPlayerList;
+
+    // create random players to test
+    private Player player1 = new Player("John", null, 500,0, null, null);
+    private Player player2 = new Player("Karen", null, 600,0,null,null);
+    private Player player3 = new Player("Jake", null, 800, 0, null, null);
+    private Player player4 = new Player("Chad", null, 100,0,  null, null);
+
 
     public LeaderboardViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is leaderboard fragment");
+        sortedPlayerList = new ArrayList<Player>();
+        sortedPlayerList.add(player1);
+        sortedPlayerList.add(player2);
+        sortedPlayerList.add(player3);
+        sortedPlayerList.add(player4);
+    }
+
+    /**
+     * Reference: https://www.javatpoint.com/insertion-sort-in-java
+     * sort array of players based on score using insertion sort
+     */
+    public void sortPlayer_list(){
+        int len = sortedPlayerList.size();
+        int i;
+        int k;
+        for(i = 1; i<len; i++) {
+            key = sortedPlayerList.get(i);
+            k = i-1;
+            while (k>=0 && sortedPlayerList.get(k).getTotalScore() < key.getTotalScore()){
+                temp = sortedPlayerList.get(k);
+                sortedPlayerList.set(k+1, temp);
+                k = k -1;
+            }
+            sortedPlayerList.set(k+1, key);
+        }
+
+    }
+
+    public ArrayList<Player >getPlayerList(){
+        return sortedPlayerList;
     }
 
     public LiveData<String> getText() {
