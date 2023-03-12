eac104d90687a83386e1e6a8d997f87b2937dd98
Sukhnoor-K <skhehra@ualberta.ca>
2023-03-12 14:27:49 -0600

    Added comments and JavaDocs

--- app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchArrayAdapter.java
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchArrayAdapter.java
@@ -16,13 +16,26 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-/*** This class is used to create the listview for the search results in the leaderboard fragment
- *  It is used in LeaderboardSearchFragment.java
+/**
+ * Represents the search fragment of the leaderboard page for the list of players
  */
 public class LeaderboardSearchArrayAdapter extends ArrayAdapter {
+    /**
+     * Creates the view for the leaderboard search fragment player list
+     * @param context - the context
+     * @param players - the list of players
+     */
     public LeaderboardSearchArrayAdapter(Context context, ArrayList<Player> players) {
         super(context, 0, players);
     }
+
+    /**
+     * Gets the view for the leaderboard search fragment player list
+     * @param position - the position
+     * @param convertView - the view to convert
+     * @param parent - the parent view group
+     * @return the view for the leaderboard search fragment player list
+     */
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup
@@ -46,17 +59,16 @@ public class LeaderboardSearchArrayAdapter extends ArrayAdapter {
         if (player != null) {
             playerName.setText(player.getUsername());
             score.setText(Integer.toString(player.getTotalScore()));
-            String url_user = "https://api.dicebear.com/5.x/avataaars-neutral/png?seed=" + player.getUsername();
-            Picasso.get().load(url_user).into(image1);
+            String url_user = "https://api.dicebear.com/5.x/avataaars-neutral/png?seed=" + player.getUsername(); // sets the unique profile image for each user
+            Picasso.get().load(url_user).into(image1); // loads the image into the imageview to make it circle
 
         }
-        // set the rank of the player
+        // if the player is null, hide the cardview
         else{
             androidx.cardview.widget.CardView cardView = view.findViewById(R.id.profileCardView);
             cardView.setVisibility(View.GONE);
         }
 
-        //rank.setText("#" + Integer.toString(position+4));
-        return view;
+        return view; // return the view for the leaderboard search fragment player list
     }
 }
