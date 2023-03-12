b45fb70bb9e4777a2a47f600d5b7f939b45da03e
Sukhnoor-K <skhehra@ualberta.ca>
2023-03-11 23:27:02 -0700

    Fixed duplicate players in results and added profile picture to view

--- app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchArrayAdapter.java
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchArrayAdapter.java
@@ -5,15 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.example.binarybandits.R;
 import com.example.binarybandits.models.Player;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
+/*** This class is used to create the listview for the search results in the leaderboard fragment
+ *  It is used in LeaderboardSearchFragment.java
+ */
 public class LeaderboardSearchArrayAdapter extends ArrayAdapter {
     public LeaderboardSearchArrayAdapter(Context context, ArrayList<Player> players) {
         super(context, 0, players);
@@ -23,6 +28,7 @@ public class LeaderboardSearchArrayAdapter extends ArrayAdapter {
     public View getView(int position, View convertView, @NonNull ViewGroup
             parent) {
         View view;
+        // check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_leaderboard_search_items,
                     parent, false);
@@ -34,12 +40,21 @@ public class LeaderboardSearchArrayAdapter extends ArrayAdapter {
         Player player = (Player) getItem(position);
         TextView playerName = view.findViewById(R.id.player_name_text);
         TextView score = view.findViewById((R.id.score_Player));
-        //TextView rank = view.findViewById(R.id.rank_number);
+        ImageView image1 = view.findViewById(R.id.leaderboard_image_list);
+
+        // set the text and image for each item
         if (player != null) {
             playerName.setText(player.getUsername());
             score.setText(Integer.toString(player.getTotalScore()));
-        }
+            String url_user = "https://api.dicebear.com/5.x/avataaars-neutral/png?seed=" + player.getUsername();
+            Picasso.get().load(url_user).into(image1);
 
+        }
+        // set the rank of the player
+        else{
+            androidx.cardview.widget.CardView cardView = view.findViewById(R.id.profileCardView);
+            cardView.setVisibility(View.GONE);
+        }
 
         //rank.setText("#" + Integer.toString(position+4));
         return view;
--- app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchFragment.java
+++ app/src/main/java/com/example/binarybandits/ui/leaderboard/LeaderboardSearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.binarybandits.ui.leaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.binarybandits.R;
@@ -24,15 +28,15 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LeaderboardSearchFragment extends Fragment {
-
-    //private EditText searchInput;
-    //private ListView searchResults;
 
     public LeaderboardSearchFragment() {
         // Required empty public constructor
@@ -44,184 +48,83 @@ public class LeaderboardSearchFragment extends Fragment {
 
         View search = inflater.inflate(R.layout.fragment_leaderboard_search, container, false);
 
-        /*DBConnector dbConnector = new DBConnector();
-        PlayerDB playerDB = new PlayerDB(dbConnector);
-        ArrayList<Player> players = new ArrayList<>();
-        //ArrayList<String> usernames = new ArrayList<>();
-        //ArrayList<Integer> scores = new ArrayList<>();
 
-        searchInput = search.findViewById(R.id.search_bar);
-        String searchText = searchInput.getText().toString();
-
-        if ((searchText).length() != 0) {
-            playerDB.getPlayer(searchText, new PlayerCallback() {
-                @Override
-                public void onPlayerCallback(Player searchedPlayer) {
-                    players.add(searchedPlayer);
-
-                }
-            });
-
-            // display all players in ListView
-            searchResults = search.findViewById(R.id.search_results);
-
-            ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), players);
-            searchResults.setAdapter(playerArrayAdapter);
-
-
-        }
-        return search;
-    }
-}*/
-
-
-        /*DBConnector dbConnector = new DBConnector();
-        PlayerDB playerDB = new PlayerDB(dbConnector);
-        ArrayList<Player> players = new ArrayList<>();
-
-        EditText searchInput = search.findViewById(R.id.search_bar);
-        ListView searchResults = search.findViewById(R.id.search_results);
-
-        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    String searchText = searchInput.getText().toString();
-
-                    if (!searchText.isEmpty()) {
-                        playerDB.getPlayer(searchText, new PlayerCallback() {
-                            @Override
-                            public void onPlayerCallback(Player searchedPlayer) {
-                                players.add(searchedPlayer);
-                                ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), players);
-                                searchResults.setAdapter(playerArrayAdapter);
-                            }
-                        });
-                    } else {
-                        players.clear();
-                        ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), players);
-                        searchResults.setAdapter(playerArrayAdapter);
-                    }
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        return search;
-    }*/
         DBConnector dbConnector = new DBConnector();
         PlayerDB playerDB = new PlayerDB(dbConnector);
-        ArrayList<Player> players = new ArrayList<>();
+        Set<Player> players = new HashSet<>(); // change ArrayList to Set to avoid duplicate results
 
         EditText searchInput = search.findViewById(R.id.search_bar);
         ListView searchResults = search.findViewById(R.id.search_results);
 
-        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH || event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    if (event.getAction() == KeyEvent.ACTION_UP) {
-                        String searchText = searchInput.getText().toString();
 
-                        players.clear();
-
-                        if (!searchText.isEmpty()) {
-                            playerDB.getPlayer(searchText, new PlayerCallback() {
-                                @Override
-                                public void onPlayerCallback(Player searchedPlayer) {
-                                    players.add(searchedPlayer);
-                                    ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), players);
-                                    searchResults.setAdapter(playerArrayAdapter);
-                                }
-                            });
-                        } else {
-                            players.clear();
-                            ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), players);
-                            searchResults.setAdapter(playerArrayAdapter);
-                        }
-                    }
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        return search;
-    }}
-
-
-        //ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, usernames);
-            //searchResults.setAdapter(adapter);
-
-
-        //}
-
-
-
-
-        //ArrayAdapter<Player> playerArrayAdapter = new LeaderboardArrayAdapter(getActivity(), players);
-        //listView.setAdapter(playerArrayAdapter);
-
-
-    /*return search;
-
-
-        searchInput = search.findViewById(R.id.search_bar);
-        searchResults = search.findViewById(R.id.search_results);
-        db = FirebaseFirestore.getInstance();
+        // Test listening to text changes, and updating the listview when the text changes
         searchInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // not needed
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                search(s.toString());
+                players.clear(); // clear Set before adding new players
+                String searchText = s.toString();
+
+                // If the search text is not empty, search for the player
+                if (!searchText.isEmpty()) {
+                    playerDB.getPlayer(searchText, new PlayerCallback() {
+                        // When the player is found, add it to the listview
+                        @Override
+                        public void onPlayerCallback(Player searchedPlayer) {
+                            players.add(searchedPlayer); // use Set.add() instead of ArrayList.add()
+                            ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), new ArrayList<>(players)); // convert Set to ArrayList
+                            searchResults.setAdapter(playerArrayAdapter);
+
+                        }
+                    });
+                }
+                // If the search text is empty, clear the listview
+                else {
+                    players.clear();
+                    //ArrayAdapter<Player> playerArrayAdapter = new LeaderboardSearchArrayAdapter(getActivity(), new ArrayList<>(players)); // convert Set to ArrayList
+                    //searchResults.setAdapter(playerArrayAdapter);
+                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
+                // not needed
+            }
+        });
+
+        // When the user presses the search button on the keyboard, search for the player
+        Button backToLeaderboard = search.findViewById(R.id.back_to_leaderboard);
+        backToLeaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToLeaderboard(v);
+            }
+        });
+
+        searchResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(LeaderboardSearchFragment.this.getActivity(), otherProfileActivity.class);
+                startActivity(intent);
+
             }
         });
 
         return search;
-    }*/
 
-    /*private void search(String query) {
-        CollectionReference collectionRef = db.collection("Players");
-        Query searchQuery = collectionRef.whereEqualTo("username", query);
+    }
 
-        searchQuery.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                List<DocumentSnapshot> results = task.getResult().getDocuments();
-                displayResults(results);
-            } else {
-                // handle errors here
-            }
-        });
-    }*/
+    // This method is called when the back to leaderboard button is clicked
+    public void backToLeaderboard(View view) {
+        //on click back to leaderboard
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LeaderboardFragment()).commit();
+
+    }
 
 
 
-        /*private void displayResults(List<DocumentSnapshot> results) {
-        List<String> usernames = new ArrayList<>();
-        //List<String> names = new ArrayList<>();
-        for (DocumentSnapshot document : results) {
-            usernames.add(document.getString("username"));
-            //names.add(document.getString("name"));
-            //names.add(document.getString("points"));
-
-        }
-        // display the names in a ListView
-        //LeaderboardViewModel leaderboardViewModel = new LeaderboardViewModel();
-        //ArrayList<Player> names = leaderboardViewModel.getPlayerList();
-        ArrayAdapter<Player> playerArrayAdapter = new LeaderboardArrayAdapter(getActivity(), usernames);
-
-        searchResults.setAdapter(playerArrayAdapter);
-        //ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, usernames);
-        //searchResults.setAdapter(adapter);
-    }*/
-
-
-       // }
+}
