e1e96e359deb4a8320ca107649e371ba6b361b70
Vera <vmaoued@gmail.com>
2023-03-13 12:30:50 -0600

    MainActivity test cases

--- app/src/androidTest/java/com/example/binarybandits/MainActivityTest.java
+++ app/src/androidTest/java/com/example/binarybandits/MainActivityTest.java
@@ -1,11 +1,14 @@
 package com.example.binarybandits;
 
+import static org.junit.Assert.assertTrue;
+
 import android.app.Activity;
 import android.app.Fragment;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.binarybandits.qrcode.QRCodeInfoActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.robotium.solo.Solo;
 import org.junit.After;
@@ -29,6 +32,7 @@ public class MainActivityTest {
      */
     @Before
     public void setUp() throws Exception {
+
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
 
@@ -41,25 +45,23 @@ public class MainActivityTest {
     public void start() throws Exception {
         Activity activity = rule.getActivity();
     }
-
-    /**
-     * Add a city to the listview and check the city name using assertTrue
-     * Clear all the cities from the listview and check again with assertFalse
-     */
-    @Test
-    public void checkBottomNavBar(){
-        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-
-        Activity current = solo.getCurrentActivity();
-        Fragment mainFragment = current.getFragmentManager().findFragmentById(R.id.main_fragment);
+//
+//    /**
+//     * Add a city to the listview and check the city name using assertTrue
+//     * Clear all the cities from the listview and check again with assertFalse
+//     */
+//    @Test
+//    public void checkBottomNavBar(){
+//        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+//        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+//
+//        Activity current = solo.getCurrentActivity();
 //        Fragment mainFragment = current.getFragmentManager().findFragmentById(R.id.main_fragment);
-        BottomNavigationView navBar = (BottomNavigationView) solo.getView(R.id.nav_view);
-
-        solo.clickOnView(navBar.findViewById(R.id.navigation_home));
-    }
-
-
+////        Fragment mainFragment = current.getFragmentManager().findFragmentById(R.id.main_fragment);
+//        BottomNavigationView navBar = (BottomNavigationView) solo.getView(R.id.nav_view);
+//
+//
+//    }
 
     /**
      * Checks functionality of scan button
@@ -67,10 +69,45 @@ public class MainActivityTest {
     @Test
     public void testScanner() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-
+        assertTrue(solo.waitForText("scanning", 1, 2000));
         solo.clickOnView(solo.getView(R.id.scan_button));
         solo.assertCurrentActivity("Should be second activity", ScanQRActivity.class);
     }
+
+
+    /**
+     * Check if profile page is displayed
+     */
+    @Test
+    public void checkProfilePage(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        BottomNavigationView navBar = (BottomNavigationView) solo.getView(R.id.nav_view);
+        solo.clickOnView(navBar.findViewById(R.id.navigation_profile));
+        assertTrue(solo.waitForText("Profile", 1, 2000));
+
+    }
+
+    /**
+     * Check if profile page is displayed
+     */
+    @Test
+    public void checkLeaderboardPage(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        BottomNavigationView navBar = (BottomNavigationView) solo.getView(R.id.nav_view);
+        solo.clickOnView(navBar.findViewById(R.id.navigation_leaderboard));
+        assertTrue(solo.waitForText("Leaderboard", 1, 2000));
+
+    }
+
+    @Test
+    public void checkMapsPage(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        BottomNavigationView navBar = (BottomNavigationView) solo.getView(R.id.nav_view);
+        solo.clickOnView(navBar.findViewById(R.id.navigation_maps));
+        assertTrue(solo.waitForText("Maps", 1, 2000));
+
+    }
+
 
     /**
      * Closes the activity after each test
