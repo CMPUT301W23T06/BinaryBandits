e196b171217207ad4a00dbf6b884599358ac2b57
AdvikMehta <advikmehta444@yahoo.com>
2023-04-01 17:14:39 -0600

    changed backend, such that location image is now saved as QR code name + username, to stay unique for each user and qr code pair

--- app/src/main/java/com/example/binarybandits/ui/QRedit/QRCodeEditActivity.java
+++ app/src/main/java/com/example/binarybandits/ui/QRedit/QRCodeEditActivity.java
@@ -86,6 +86,7 @@ public class QRCodeEditActivity extends AppCompatActivity {
         String name = qrController.generateUniqueName(hash);
         int points = qrController.calculatePoints(hash);
         nameTextView.setText(name);
+        String username = AuthController.getUsername(QRCodeEditActivity.this);
         String pointsString = points + " point(s) collected!";
         pointsTextView.setText(pointsString);
         DownloadImageTask.loadQRImageIntoView(imageView, hash);
@@ -99,7 +100,7 @@ public class QRCodeEditActivity extends AppCompatActivity {
                 PlayerDB db = new PlayerDB(new DBConnector());
                 if (photo != null) {
                     QRCodeDB qrCodeDB = new QRCodeDB(new DBConnector());
-                    qrCodeDB.addLocationImageToServer(photo, name);
+                    qrCodeDB.addLocationImageToServer(photo, name+username);
                 }
                 db.getPlayer(uid, new PlayerCallback() {
                     @Override
