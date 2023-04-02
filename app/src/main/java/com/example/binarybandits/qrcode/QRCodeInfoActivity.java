f49f976acdd9e1cb4f43503dda4acbbfc84f09fd
alexrnguyen <alexander.nguyen@shaw.ca>
2023-04-02 13:09:20 -0600

    Bug fix for highest score not updated on deletion of QR code

--- app/src/main/java/com/example/binarybandits/qrcode/QRCodeInfoActivity.java
+++ app/src/main/java/com/example/binarybandits/qrcode/QRCodeInfoActivity.java
@@ -28,7 +28,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.binarybandits.DBConnector;
 import com.example.binarybandits.MainActivity;
 import com.example.binarybandits.R;
+import com.example.binarybandits.ScoreCallback;
 import com.example.binarybandits.controllers.AuthController;
+import com.example.binarybandits.controllers.PlayerController;
 import com.example.binarybandits.models.Comment;
 import com.example.binarybandits.models.Player;
 import com.example.binarybandits.models.QRCode;
@@ -259,6 +261,13 @@ public class QRCodeInfoActivity extends AppCompatActivity {
                                                     player.decrementTotalQRCodes();
                                                     int newScore = player.getTotalScore() - qrCode.getPoints();
                                                     player.setTotalScore(newScore);
+                                                    PlayerController controller = new PlayerController(player);
+                                                    controller.getHighestQRCode(new ScoreCallback() {
+                                                        @Override
+                                                        public void scoreCallback(int score) {
+                                                            player.setHighestScore(score);
+                                                        }
+                                                    });
                                                     db_player.updatePlayer(player);
                                                 }
                                             });
